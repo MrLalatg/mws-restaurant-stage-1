9633603fe28f92e2241c8be4271652b0b434d9f4
Ilya Zabludovskiy <MrLalatg@gmail.com>
2018-08-22 18:19:24 +0300

    feat: added api key

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoibXJsYWxhdGciLCJhIjoiY2psNTc5dnNrMDBtbTN2cnZrdW5uNnN4aSJ9.mmq_eDCmNC2cSkOt475iZA',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
